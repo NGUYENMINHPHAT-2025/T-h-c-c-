cd691b6394920f9b02140a854bc337af56935f5f
NGUYENMINHPHAT <nminhphat2025github@gmail.com>
2025-07-24 22:41:37 +0700

    Add files via upload

---
+++ Buổi 1_c++_lệnh in ra màng hình.cpp
@@ -0,0 +1,20 @@
+//  In ra dòng chữ trên màn hình (console)
+
+#include <iostream> // thư viện để in ra màng hình
+using namespace std; // sử dụng không gian tên std
+
+int main() {
+    cout << "hello phát, chào mừng bạn đến với C++!\n"; // in ra dòng chữ
+    cout << "Hãy bắt đầu hành trình lập trình của bạn!\n"; // in thêm một dòng nữa
+    cout << "Chúc bạn học tốt và vui vẻ với C++!\n"; // in thêm một lời chúc
+    return 0; // trả về 0 để kết thúc chương trình
+}
+
+
+// #include <iostream> --> Nạp thư viện để dùng cout (in ra màn hình)
+// using namespace std; -->	Cho phép dùng cout thay vì std::cout
+// int main() --> Hàm chính - nơi bắt đầu chương trình
+// cout << ".......\n"; --> In ra dòng chữ
+// return 0; --> Kết thúc chương trình và trả về giá trị 0
+
+
